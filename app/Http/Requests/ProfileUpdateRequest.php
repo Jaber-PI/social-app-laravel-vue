ca68475683efc86ae8c15fe37b671cc1b4963c7f
jaber-PI <jaberessaber@gmail.com>
2024-11-03 14:57:08 +0000

    add username to user profile and allow user to edit it

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,6 +16,13 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'username' => [
+                'required',
+                'string',
+                'max:255',
+                'regex:/^(?![_-])[A-Za-z0-9_-]{3,255}$/',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
             'name' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
@@ -25,6 +32,17 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'username.required' => 'The username field is required.',
+            'username.string' => 'The username must be a valid string.',
+            'username.regex' => 'The username must be between 3 and 255 characters long and can only contain letters, numbers, underscores, and dashes.',
+            'username.max' => 'The username may not be greater than 255 characters.',
+            'username.unique' => 'The username has already been taken. Please choose a different one.',
         ];
     }
 }
